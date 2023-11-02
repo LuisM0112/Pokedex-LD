25514af671c8e7d3fe37a24d4a2150146b40a14a
LuisM0112 <149406770+LuisM0112@users.noreply.github.com>
2023-11-02 14:50:55 +0100

    Funcion request pokemon y fetch all pokemon

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -1,15 +1,28 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable } from 'rxjs';
+import { Observable, forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators'
 import { Pokemon } from './model/pokemon';
 @Injectable({
   providedIn: 'root'
 })
-export class PokemonService implements OnInit {
+export class PokemonService {
   pokemones: Pokemon[] = [];
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {
+    // for (let i= 0; i < 493; i++){
+    //   this.pokemon = this.requestPokemon(i+1).subscribe(
+    //     (newPokemon) => {
+    //       this.addPokemon(newPokemon);
+    //     },
+    //     (error) => {
+    //       console.error('Error al obtener Pokémon:', error);
+    //     }
+    //   );
+    //   this.addPokemon(new Pokemon());
+    // }
+    this.fetchAllPokemon();
+  }
   
   getPokemones(): Pokemon[] {
     return this.pokemones;
@@ -19,12 +32,40 @@ export class PokemonService implements OnInit {
     return <Pokemon>{ ...this.pokemones.find(t => t.id == id) };
   }
 
-  requestPokemon(id: number): Observable<string> {
-    return this.http.get('https://pokeapi.co/api/v2/pokemon/'+id)
-    .pipe(map((request: any) => request.name));
+  fetchAllPokemon() {
+    const requests: Observable<Pokemon>[] = [];
+    for (let i = 1; i <= 493; i++) {
+      const request = this.requestPokemon(i);
+      requests.push(request);
+    }
+
+    forkJoin(requests).subscribe(
+      (pokemones: Pokemon[]) => {
+        this.pokemones = pokemones;
+      },
+      (error) => {
+        console.error('Error fetching Pokemon:', error);
+      }
+    );
   }
-  
-  ngOnInit(): void {
-      
+
+  requestPokemon(id: number): Observable<Pokemon> {
+    return this.http.get('https://pokeapi.co/api/v2/pokemon/&{id}').pipe(
+      map((response: any) => {
+        const pokemon = new Pokemon();
+        pokemon.id = response.id;
+        pokemon.name = response.name;
+        pokemon.description = response.description;
+        pokemon.type1 = response.type1;
+        pokemon.type2 = response.type2;
+        pokemon.weight = response.weight;
+        return pokemon;
+      })
+    );
   }
+
+  // addPokemon(pokemon: Pokemon){
+  //   this.pokemones.push(pokemon);
+  // }
+
 }
