ec2efa0f29e73f3113638eee2f0422067f2b23f0
LuisM0112 <149406770+LuisM0112@users.noreply.github.com>
2023-11-04 19:14:33 +0100

    Merge Dani + Types Colors for Background and types
    El gradiente del fondo será según su tipo y las etiquetas de los tipos también

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -23,4 +23,37 @@ export class PokemonListComponent implements OnInit {
   getPokemon(id: number): Pokemon {
     return <Pokemon>{ ...this.pokemones.find(t => t.id == id) };
   }
+
+  typeColors: { [key: string]: string } = {
+    default: 'var(--default)',
+    normal: 'var(--normal)',
+    fire: 'var(--fire)',
+    water: 'var(--water)',
+    grass: 'var(--grass)',
+    electric: 'var(--electric)',
+    ice: 'var(--ice)',
+    fighting: 'var(--fighting)',
+    poison: 'var(--poison)',
+    ground: 'var(--ground)',
+    flying: 'var(--flying)',
+    psychic: 'var(--psychic)',
+    bug: 'var(--bug)',
+    rock: 'var(--rock)',
+    ghost: 'var(--ghost)',
+    dragon: 'var(--dragon)',
+    dark: 'var(--dark)',
+    steel: 'var(--steel)',
+    fairy: 'var(--fairy)',
+  };
+
+  getGradientBackground(pokemon: Pokemon): { [key: string]: string } {
+    const backgroundStyle: { [key: string]: string } = {};
+    
+    if (pokemon.type1 && pokemon.type2) {
+      backgroundStyle['background'] = `radial-gradient(${this.typeColors[pokemon.type1]} 0%, ${this.typeColors[pokemon.type2]} 100%)`;
+    } else backgroundStyle['background'] = `radial-gradient(${this.typeColors[pokemon.type1]} 0%, ${this.typeColors['default']} 100%)`;
+
+  
+    return backgroundStyle;
+  }
 }
