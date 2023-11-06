81e1db16bf0283bab3e5e9710610788cf3fd662c
Ajolote41 <149406770+LuisM0112@users.noreply.github.com>
2023-11-06 21:02:17 +0100

    Animación id + refactorización typeColors

--- src/app/pokemon-list/pokemon-list.component.ts
+++ src/app/pokemon-list/pokemon-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Pokemon } from '../model/pokemon';
 import { PokemonService } from '../pokemon.service';
+import * as jsonData from '../../assets/data/typeColors.json';
 
 @Component({
   selector: 'app-pokemon-list',
@@ -9,6 +10,7 @@ import { PokemonService } from '../pokemon.service';
 })
 export class PokemonListComponent implements OnInit {
   pokemones: Pokemon[] = [];
+  typeColors: any = jsonData;
 
   constructor(public pokemonService: PokemonService) {}
   
@@ -24,34 +26,12 @@ export class PokemonListComponent implements OnInit {
     return <Pokemon>{ ...this.pokemones.find(t => t.id == id) };
   }
 
-  typeColors: { [key: string]: string } = {
-    default: 'var(--default)',
-    normal: 'var(--normal)',
-    fire: 'var(--fire)',
-    water: 'var(--water)',
-    grass: 'var(--grass)',
-    electric: 'var(--electric)',
-    ice: 'var(--ice)',
-    fighting: 'var(--fighting)',
-    poison: 'var(--poison)',
-    ground: 'var(--ground)',
-    flying: 'var(--flying)',
-    psychic: 'var(--psychic)',
-    bug: 'var(--bug)',
-    rock: 'var(--rock)',
-    ghost: 'var(--ghost)',
-    dragon: 'var(--dragon)',
-    dark: 'var(--dark)',
-    steel: 'var(--steel)',
-    fairy: 'var(--fairy)',
-  };
-
   getGradientBackground(pokemon: Pokemon): { [key: string]: string } {
     let backgroundStyle: { [key: string]: string } = {};
     
     if (pokemon.type1 && pokemon.type2) {
       backgroundStyle['background'] = `radial-gradient(${this.typeColors[pokemon.type2]} 0%, ${this.typeColors[pokemon.type1]} 100%)`;
-    } else backgroundStyle['background'] = `radial-gradient(${this.typeColors[pokemon.type1]} 0%, ${this.typeColors[pokemon.type1]} 40%,  ${this.typeColors['default']} 100%)`;
+    } else backgroundStyle['background'] = `radial-gradient(${this.typeColors[pokemon.type1]} 0%, ${this.typeColors[pokemon.type1]} 40%, ${this.typeColors["null"]} 100%)`;
 
     return backgroundStyle;
   }
