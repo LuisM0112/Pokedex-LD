2eabba5234e29c6d749980ae1ade8bd05027e0f3
LuisM0112 <luismigase@gmail.com>
2023-11-18 21:22:31 +0100

    refactorización calculo generaciones + petición de descripción

--- src/app/pokemon.service.ts
+++ src/app/pokemon.service.ts
@@ -4,6 +4,8 @@ import { Observable, forkJoin } from 'rxjs';
 import { map } from 'rxjs/operators'
 import { Pokemon } from './model/pokemon';
 
+import * as jsonGens from '../assets/data/generationsData.json';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -13,6 +15,9 @@ import { Pokemon } from './model/pokemon';
  * to get the list of pokemon to be displayed
  */
 export class PokemonService {
+
+  gensData: any = jsonGens;
+  generationLimits: number[] = this.gensData.generations.map((gen: any) => gen.limit);
 
   constructor(private http: HttpClient) {}
 
@@ -47,29 +52,7 @@ export class PokemonService {
         type2: response.types[1] ? response.types[1].type.name : '',
         height: response.height/10,
         weight: response.weight/10,
-        generation: this.calculateGeneration(response.id),
-        hp: response.stats[0].base_stat,
-        attack: response.stats[1].base_stat,
-        defense: response.stats[2].base_stat,
-        specialAttack: response.stats[3].base_stat,
-        specialDefense: response.stats[4].base_stat,
-        speed: response.stats[5].base_stat,
-      }))
-    );
-  }
-  // 29
-  requestPokemonDescription(id: number): Observable<Pokemon> {
-    return this.http.get('https://pokeapi.co/api/v2/pokemon-species/'+id+'/').pipe(
-      map((response: any) => ({
-        id: response.id,
-        spriteNormal: response.sprites.other['official-artwork'].front_default,
-        spriteShiny: response.sprites.other['official-artwork'].front_shiny,
-        name: response.name,
-        type1: response.types[0] ? response.types[0].type.name : '',
-        type2: response.types[1] ? response.types[1].type.name : '',
-        height: response.height/10,
-        weight: response.weight/10,
-        generation: this.calculateGeneration(response.id),
+        generation: this.getGeneration(response.id),
         hp: response.stats[0].base_stat,
         attack: response.stats[1].base_stat,
         defense: response.stats[2].base_stat,
@@ -80,27 +63,24 @@ export class PokemonService {
     );
   }
 
-  private calculateGeneration(id: number): number {
+  requestPokemonDescription(id: number): Observable<string> {
+    return this.http.get(`https://pokeapi.co/api/v2/pokemon-species/${id}/`).pipe(
+      map((response: any) => {
+        const descriptionObj = response.flavor_text_entries.find((entry: any) => entry.language.name === 'en');
+        const description = descriptionObj ? descriptionObj.flavor_text : 'No description available';
+        return description;
+      })
+    );
+  }
+
+  private getGeneration(id: number): number {
     let result: number = 0;
-    if (id <= 151) {
-      result = 1;
-    } else if (id <= 251) {
-      result = 2;
-    } else if (id <= 386) {
-      result = 3;
-    } else if (id <= 493) {
-      result = 4;
-    } else if (id <= 649) {
-      result = 5;
-    } else if (id <= 721) {
-      result = 6;
-    } else if (id <= 809) {
-      result = 7;
-    } else {
-      result = 8;
+    for (let i = 0; i < this.generationLimits.length; i++) {
+      if (id <= this.generationLimits[i]) {
+        result = i + 1;
+        break;
+      }
     }
     return result;
   }
-
 }
-
